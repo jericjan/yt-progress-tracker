fa55708db75cc863c774c755860d3a98c5e5cce1
jericjan <cantilfrederick@gmail.com>
2024-04-16 15:30:51 +0800

    - anime.js on options page

--- src/options.tsx
+++ src/options.tsx
@@ -1,10 +1,41 @@
-import React from "react";
+import React, { useEffect } from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import "./options.css";
 import { swalConfirm, swalBasic } from "modules/swal";
+import anime from "animejs";
 
 function Options() {
+  useEffect(() => {
+    anime({
+      targets: "#big-ass-button",
+      translateY: [
+        { value: "131vh", easing: "steps(1)" },
+        { value: "0vh", easing: "easeOutElastic" },
+      ],
+      scaleY: [
+        { value: "5", easing: "steps(1)" },
+        { value: "1", easing: "easeOutElastic" },
+      ],
+      rotate: [
+        { value: "0", delay: 1500 },
+        { value: "360", duration: 1000, easing: "easeInOutBack" },
+      ],
+      "background-color": [
+        { value: "#eceff4", delay: 2250, easing: "easeOutSine" },
+        { value: "#ff5757", duration: 500 },
+        { value: "#eceff4", duration: 500 },
+      ],
+      opacity: [{value: 0, duration: 0}, {value: 1,duration: 3000}],
+      easing: "linear",
+      duration: 3000,
+    });
+
+    return function () {
+      anime.remove("#big-ass-button");
+    };
+  }, []);
+
   const click = () => {
     swalConfirm(
       "All video progress data will be deleted.",
